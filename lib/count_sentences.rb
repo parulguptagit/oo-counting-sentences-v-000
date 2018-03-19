5d5068a6620dc5ad67731c5e612d473fb92cb896
Parul Gupta <parulguptasmail@gmail.com>
2018-03-19 21:49:56 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.scan(/[.]\z/).empty? ? FALSE : TRUE
   end
 
   def question?
-
+    self.scan(/[?]\z/).empty? ? FALSE : TRUE
   end
 
   def exclamation?
-
+    self.scan(/[!]\z/).empty? ? FALSE : TRUE
   end
 
   def count_sentences
-
+    self.scan(/[a-zA-Z0-9][!?.]/).count
   end
 end
